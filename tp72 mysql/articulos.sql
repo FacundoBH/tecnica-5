eed1aa7b23ada8350051fbe2274acfc47307053d
FacundoBH <102985869+FacundoBH@users.noreply.github.com>
2022-08-19 09:56:18 -0300

    Add files via upload

---
+++ tp72 mysql/articulos.sql
@@ -0,0 +1,11 @@
+INSERT INTO articulos (idarticulo, nombrearticulo, precioarticulo, idfabricante) VALUES
+  (1, 'Teclado  ', 1000, 3),
+  (2, 'Hdd 730 Gb', 5000, 5),
+  (3, 'Mouse  ', 800, 3),
+  (4, 'Memoria USB', 1400, 4),
+  (5, 'Memoria RAM', 2900, 1),
+  (6, 'Disco duro extraíble 800 Gb', 6500, 5),
+  (7, 'Memoria USB', 2790, 1),
+  (8, 'DVD Rom', 4500, 2),
+  (9, 'CD Rom', 2000, 2),
+  (10, 'Tarjeta de red', 1800, 3);
