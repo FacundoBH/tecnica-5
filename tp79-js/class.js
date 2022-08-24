a1e401c10ee59edead17946a4dbd4357b10032bc
FacundoBH <102985869+FacundoBH@users.noreply.github.com>
2022-08-24 09:04:01 -0300

    Add files via upload

---
+++ tp79-js/class.js
@@ -0,0 +1,11 @@
+class CRectangulo{
+    constructor(base,altura){
+        this.base = base;
+        this.altura = altura;
+    }
+    CalclarArea(){
+        return this.base*this.altura;
+    }
+}
+const rectangulo = new CRectangulo(4.8, 5.8)
+document.write("Base: " + rectangulo.base + "<br>" + "Altura: " + rectangulo.altura + "<br>" + "Area: " + rectangulo.area());
